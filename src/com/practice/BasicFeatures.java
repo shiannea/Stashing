cbd22cf233a868516a36e4427a1983abfa8c4382
shiannea <shianne.ali99@gmail.com>
2022-05-15 12:05:22 -0400

    the red Audi car is ready with basic features

--- src/com/practice/BasicFeatures.java
+++ src/com/practice/BasicFeatures.java
@@ -1,6 +1,11 @@
 package com.practice;
 
 public class BasicFeatures {
+    String engineCapac;
+    String Color;
+    String doors;
+    String headlights;
+    String regNo;
 
     public static void main(String[] args) {
 
--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("stashing");
-        BasicFeatures basic=new BasicFeatures();
+        BasicFeatures Auddi=new BasicFeatures();
+        Auddi.Color="red";
+        Auddi.doors="4";
+        Auddi.engineCapac="V8";
+        Auddi.headlights="blue";
+        Auddi.regNo="tuzik";
     }
 }
